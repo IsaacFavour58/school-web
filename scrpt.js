f702a70ae835bee03ccb0767bffa443134a80d85
isaac favour <isaacfavour5858@gmail.com>
2023-03-23 20:02:41 -0500

    worked on java-script

---
+++ scrpt.js
@@ -0,0 +1,26 @@
+// variable names
+const navlinks = document.querySelector('.navlinks');
+const navlinksEl = document.querySelectorAll('.navlinks ul');
+const navOpen = document.querySelector('.open');
+const navClose = document.querySelector('.close');
+
+function openNav(){
+  navlinks.style.top = "30%";
+  navlinks.style.left = "60%";
+  navOpen.style.display = "none";
+  navClose.style.display = "block";
+}
+
+
+function closeNav(){
+  navlinks.style.top = "-80%";
+  navlinks.style.left = "60%";
+  navOpen.style.display = "block";
+  navClose.style.display = "none";
+}
+
+navlinksEl.forEach(navLink => {
+  navLink.addEventListener('click', function(){
+    closeNav()
+  })
+})
